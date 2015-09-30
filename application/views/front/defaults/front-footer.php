20163d052e2705157b8d06e87f23edd5f5221fe1
sohrabramzi <sohrab.ramzi1@gmail.com>
2015-09-30 11:25:39 +0200

    footer

--- application/views/front/defaults/front-footer.php
+++ application/views/front/defaults/front-footer.php
@@ -15,5 +15,19 @@
 	})
 </script>
 	</div>
+</div>
+
+	<footer class="footer">
+		<div class="footer navbar-fixed-bottom navbar-inverse"> 
+	      		<div class="container">
+	          <p class="text-muted" style="margin: 14px 0px 14px 0px;">Footer</p>
+	          <a href="https://www.youtube.com/" target="_blank"><p>lol</p></a>
+	          <a href="https://www.youtube.com/" target="_blank"><p>Programmas</p></a>
+	          </div>
+	        </div>
+	      </div>
+    </footer>
+
 </body>
+
 </html>
